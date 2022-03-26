0b053fcada31e42cac8b5c819570e2ac0fa95c06
NgTuanDung029 <rivencyber@gmail.com>
2022-03-26 14:19:14 +0700

    tai len index.html va app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const chooseCity = document.querySelector('#choose-city');
+const city = document.querySelector('.city');
+
+chooseCity.addEventListener('focusout', chooseCityCheck);
+
+function chooseCityCheck(){
+    console.log("abc");
+}
+
+
